7cb04a331e399b0087443b79c4bbcdaa555f8496
Rho <idrodrigo@icloud.com>
2023-07-25 20:20:35 -0600

    rm vercel

--- index.js
+++
@@ -1,7 +0,0 @@
-import server from './src/app.js'
-import { connectDB } from './src/db.js'
-
-const PORT = process.env.PORT || 3000
-connectDB()
-server.listen(PORT)
-console.log('Server on portt', PORT)
