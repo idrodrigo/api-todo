c6c0f43600a06d47d905fd20667015e31cc7e2c9
Rho <idrodrigo@icloud.com>
2023-07-25 20:31:58 -0600

    r

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import server from './app.js'
 import { connectDB } from './db.js'
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 0
 connectDB()
 server.listen(PORT)
 console.log('Server on portt', PORT)
